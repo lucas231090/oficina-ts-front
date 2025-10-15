f1402922f00d86c83d9c0e575f60eac72c5a44cb
Lucas <43862551+lucas231090@users.noreply.github.com>
2025-10-15 15:18:44 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { GlobalStyles } from "./styles/GlobalStyles"
+
+export default function App(){
+
+  return (
+   <>
+   <GlobalStyles />
+   <h1>
+    BEM VINDO A OFICINA DO PROF LUCAS
+  </h1>
+  </>
+  )
+
+}
